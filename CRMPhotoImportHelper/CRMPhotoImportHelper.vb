aa6caa050a8c2bb5b0e408dc3f67ea847f592c6a
Mark Sellers <mark@masenterprisesinc.com>
2013-07-25 17:41:33 -0600

    Added new functionality to perform the SFTP transfer of the image files.

--- CRMPhotoImportHelper/CRMPhotoImportHelper.vb
+++ CRMPhotoImportHelper/CRMPhotoImportHelper.vb
@@ -7,11 +7,17 @@ Imports System.Drawing.Drawing2D
 'Imports System.Drawing.Bitmap
 'Imports System.Windows.Media.Imaging
 Imports System.Configuration
+Imports System.Collections.Specialized
 Imports System.IO
 Imports System.Text
 Imports System.Text.RegularExpressions
+Imports System.Reflection
+Imports SFTP_Test
+Imports System.ComponentModel
 
 Public Class CRMPhotoImportHelper
+
+
 	<System.Runtime.InteropServices.DllImport("gdi32.dll")> _
 	Public Shared Function DeleteObject(ByVal hObject As IntPtr) As Boolean
 	End Function
@@ -38,6 +44,32 @@ Public Class CRMPhotoImportHelper
 	Private _PaddingTop As Decimal = 0
 	Private _PaddingSide As Decimal = 0
 
+	' this is the class that handles all the SFTP stuff
+	Private _sftpClient As New OcmSFTPClient()
+
+	' background worker for GUI:
+	Private _Worker As New BackgroundWorker()
+
+	' holds the SFTP specific items:
+	' ************************************************************************************************************
+	' These will need to be moved to the App.Config file before rollout!!!
+	Private _host As String = String.Empty '"files.blackbaudhosting.com"
+	Private _username As String = String.Empty '"MarkSe21195D@BBEC"
+	Private _password As String = "P@ssword2"
+	Private _workingdirectory As String = String.Empty ' "/staging/21195D/ChildPhotos"
+	Private _uploadfile As String = String.Empty ' "C:\OCM\PhotoImports\2013 Photos_Processing_6_25_2013\_Import\C210608.jpg"
+	Private _ftpPhotoFolderName As String = String.Empty
+	Private _numberOfFiles As Integer = 0
+	Private _ftpImportFileLocation As String = String.Empty
+	Private _ftpCRMPhotoFileLocation As String = String.Empty
+
+	Private _pictureType As String = String.Empty
+	Private _defaultSourceFolder As String = String.Empty
+
+	' holds the image files:
+	Private _imageFiles As String() = {}
+
+
 
 	Private Sub CRMPhotoImportHelper_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
 		e.Cancel = False
@@ -57,10 +89,26 @@ Public Class CRMPhotoImportHelper
 		Me.lblStatus.Text = ""
 
 		Me.txtMaxPhotos.Text = "5000"
-		Me.txtBlackbaudPhotoLocation.Text = "\\bbhcifs01\clientdata\Production\21195P\ChildPhotos" '"\\bbhcifs01\clientdata\staging\21195D\ChildPhotos" 
 
 		SetButtons()
 
+		' Memphis 7/19/13 for the new SFTP functionality
+		progressBar1.Maximum = 100
+		progressBar1.Style = ProgressBarStyle.Blocks
+		progressBar1.[Step] = 1
+		progressBar1.Value = 0
+		' user can't transfer until they select everything else & process the folder:
+		btnTransfer.Enabled = False
+		btnSelectFolder.Enabled = False
+
+		GetConfigEntries()
+
+		Me.txtBlackbaudPhotoLocation.Text = _ftpCRMPhotoFileLocation '"\\bbhcifs01\clientdata\Production\21195P\ChildPhotos" '"\\bbhcifs01\clientdata\staging\21195D\ChildPhotos" 
+		Me.txtImportFileFolderName.Text = _ftpImportFileLocation
+		Me.txtSourceFolderName.Text = _defaultSourceFolder
+
+		AddHandler _sftpClient.FileUploadCompleted, AddressOf fileUploadCompleted
+		AddHandler _sftpClient.FileUploading, AddressOf fileUploading
 
 	End Sub
 
@@ -76,7 +124,6 @@ Public Class CRMPhotoImportHelper
 	End Sub
 
 	Private Sub cmdProcessFolder_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdProcessFolder.Click
-
 
 		Dim parmsValid As Boolean = True
 
@@ -253,6 +300,13 @@ Public Class CRMPhotoImportHelper
 
 			lblStatus.Text = "Import file creation completed."
 
+			' fill the image files array with the image files just created & indicate to user how many were selected for transfer:
+			GetImageFiles()
+
+			If _numberOfFiles > 0 Then
+				cmdProcessFolder.Enabled = False
+			End If
+
 		End If
 	End Sub
 
@@ -344,7 +398,7 @@ Public Class CRMPhotoImportHelper
 
 	End Sub
 
-	Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+	Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
 		Dim dlg As FolderBrowserDialog
 
 		dlg = New FolderBrowserDialog()
@@ -355,5 +409,257 @@ Public Class CRMPhotoImportHelper
 
 		txtSourceFolderName.Focus()
 	End Sub
+
+#Region "EventHandlers"
+	Private Sub fileUploadCompleted(ByVal sender As Object, ByVal e As FileUploadCompletedEventArgs)
+		OutputThis("\r")
+		OutputThis("\rFinished uploading the file!")
+		OutputThis(String.Format("\rUploaded {0} {1} {2:#########}", e.UploadedFileName, e.FileNumber, e.BytesSent))
+	End Sub
+
+	Private Sub fileUploading(ByVal sender As Object, ByVal e As FileUploadingEventArgs)
+		'var backgrounder = sender as BackgroundWorker;
+		Dim progress As Integer = 100
+		'progress = (Convert.ToInt32(e.BytesSentSoFar) / Convert.ToInt32(e.FileBytesTotal)) * 100;
+		progress = CInt(Math.Truncate((CSng(e.BytesSentSoFar) / CSng(e.FileBytesTotal)) * 100))
+
+		LogThis(String.Format("Bytes sent so far: {0}", e.BytesSentSoFar))
+		LogThis(String.Format("File Bytes Total: {0}", e.FileBytesTotal))
+		LogThis(String.Format("Progress %{0}", progress))
+		LogThis(String.Format("File {0} of {1}", e.FileNumberOfTotal, e.TotalNumberFilesToTransfer))
+
+		SetControlPropertyValue(lblFilesInProgress, "text", String.Format("Transferring {0} of {1} files...", e.FileNumberOfTotal, e.TotalNumberFilesToTransfer))
+
+		BackgroundWorker1.ReportProgress(progress)
+	End Sub
+
+#End Region
+
+	Private Sub OutputThis(ByVal outputMessage As String)
+		'richTextBox1.AppendText(outputMessage)
+		LogThis(outputMessage)
+	End Sub
+
+	Private Sub LogThis(ByVal logMessage As String)
+		System.Diagnostics.Debug.WriteLine(logMessage)
+	End Sub
+
+#Region "BackgroundWorker Methods"
+	''' <summary>
+	''' Creates the BackgroundWorker with WorkerReportsProgress and 
+	''' WorkerSupportsCancellation set to true. 
+	''' Also creates the three Eventhandlers
+	''' </summary>
+	Public Sub CreateBackgroundWorker()
+		'Set WorkerReportsProgress and WorkerSupportsCancellation to true
+		_Worker.WorkerSupportsCancellation = True
+		_Worker.WorkerReportsProgress = True
+
+		'Generate the EventHandler
+		AddHandler _Worker.RunWorkerCompleted, New RunWorkerCompletedEventHandler(AddressOf Worker_RunWorkerCompleted)
+		AddHandler _Worker.ProgressChanged, New ProgressChangedEventHandler(AddressOf Worker_ProgressChanged)
+		AddHandler _Worker.DoWork, New DoWorkEventHandler(AddressOf Worker_DoWork)
+
+		_Worker.RunWorkerAsync()
+	End Sub
+
+	Private Sub Worker_DoWork(ByVal sender As Object, ByVal e As DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+		' wire up the event listeners to catch the events raised by the client:
+		AddHandler _sftpClient.FileUploadCompleted, AddressOf fileUploadCompleted
+		AddHandler _sftpClient.FileUploading, AddressOf fileUploading
+		'AddHandler _sftpClient.ListingDirectory, AddressOf listingDirectory
+
+		' setup the transfer properties needed by the client:
+		_sftpClient.HostName = _host
+		_sftpClient.UserName = _username
+		_sftpClient.Password = _password
+		_sftpClient.WorkingDirectory = _workingdirectory
+		_sftpClient.UploadFileName = _uploadfile
+		_sftpClient.PhotoFTPFolderName = FormatFTPFolderName()
+
+		' perform the transfer of a single file:
+		'_sftpClient.transferFile();
+
+		' multiple files get this call:
+		_sftpClient.transferFiles(_imageFiles)
+	End Sub
+
+	Private Sub Worker_ProgressChanged(ByVal sender As Object, ByVal e As ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
+		'Create an instace of the single file progressBar
+		Dim ProgressBarSingle As ProgressBar = DirectCast(Me.Controls("progressBar1"), ProgressBar)
+
+		'Increase the value of the progressBar by e.ProgressPercentage
+		ProgressBarSingle.Value = e.ProgressPercentage
+
+		Dim ProgressBarOverall As ProgressBar = DirectCast(Me.Controls("overallProgressBar"), ProgressBar)
+
+		'Calculate by whic the Overall ProgressBar needs to be increased,
+		'so that we get a smooth rise.
+		Dim PercentRise As Integer = 100 \ _numberOfFiles
+
+		' only increment the overall bar if this single file is done
+		If ProgressBarSingle.Value = 100 Then
+			'Duno why it has to be here, but if it isn't the progressBarSingle
+			'doesn't stop working. So leave it here with no code...
+			If ProgressBarOverall.Value >= 100 Then
+			Else
+				'Set the SingleFile progressBar to 0, so it cann be filled again
+				'ProgressBarSingle.Value = 0;
+
+				'This is the "catch" part, so the programm doesn't throw up an error
+				If ProgressBarOverall.Value + PercentRise >= 100 Then
+					'Set both progress bars to 100 so it looks better.
+					ProgressBarOverall.Value = 100
+					ProgressBarSingle.Value = 100
+				Else
+					'increase the Global progressBar by the calculated amount for each file
+					ProgressBarOverall.Value = ProgressBarOverall.Value + PercentRise
+				End If
+			End If
+		End If
+	End Sub
+
+	Private Sub Worker_RunWorkerCompleted(ByVal sender As Object, ByVal e As RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+		Dim ProgressBarSingle As ProgressBar = DirectCast(Me.Controls("progressBar1"), ProgressBar)
+		ProgressBarSingle.Value = 100
+
+		SetControlPropertyValue(overallProgressBar, "value", 100)
+
+		SetControlPropertyValue(btnTransfer, "enabled", True)
+
+		SetControlPropertyValue(lblFilesInProgress, "text", "All files transferred!")
+
+		MessageBox.Show("Image files have been transferred. You may now close this application...", "Image File Transfer", MessageBoxButtons.OK, MessageBoxIcon.Information)
+	End Sub
+#End Region
+
+	''' <summary>
+	''' These are used to manage the UI controls from the worker thread
+	''' </summary>
+	''' <param name="oControl"></param>
+	''' <param name="propName"></param>
+	''' <param name="propValue"></param>
+	Private Delegate Sub SetControlValueCallback(ByVal oControl As Control, ByVal propName As String, ByVal propValue As Object)
+	Private Sub SetControlPropertyValue(ByVal oControl As Control, ByVal propName As String, ByVal propValue As Object)
+		If oControl.InvokeRequired Then
+			Dim d As New SetControlValueCallback(AddressOf SetControlPropertyValue)
+			oControl.Invoke(d, New Object() {oControl, propName, propValue})
+		Else
+			Dim t As Type = oControl.[GetType]()
+			Dim props As PropertyInfo() = t.GetProperties()
+			For Each p As PropertyInfo In props
+				If p.Name.ToUpper() = propName.ToUpper() Then
+					p.SetValue(oControl, propValue, Nothing)
+				End If
+			Next
+		End If
+	End Sub
+
+
+	Private Sub GetImageFiles()
+		' source should be something like this:  C:\OCM\PhotoImports\2013 Photos_Processing_6_25_2013\_Import
+		_imageFiles = Directory.GetFiles(Me.txtSourceFolderName.Text + "\_Import", "*.jpg")
+		_numberOfFiles = _imageFiles.Count()
+		imageFilesSelectedLabel.Text = String.Format("{0} image files ready to transfer.", _numberOfFiles)
+		'For Each filename As String In _imageFiles
+		'	MessageBox.Show(filename)
+		'Next
+
+		If _numberOfFiles > 0 Then
+			btnTransfer.Enabled = True
+		Else
+			MessageBox.Show("Oops! Something went wrong, there are no image files to transfer!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+		End If
+
+	End Sub
+
+	Private Sub btnSelectFolder_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSelectFolder.Click
+		OpenFileDialog1.InitialDirectory = "C:\OCM\PhotoImports"
+		OpenFileDialog1.Filter = "Image Files|*.BMP;*.GIF;*.JPG;*.JPEG;*.PNG|All files (*.*)|*.*"
+		OpenFileDialog1.Title = "Select Image files to Upload"
+		OpenFileDialog1.CheckPathExists = True
+		OpenFileDialog1.Multiselect = True
+		Dim imageDialogResult As DialogResult = OpenFileDialog1.ShowDialog()
+
+		If imageDialogResult = DialogResult.OK Then
+			_imageFiles = OpenFileDialog1.FileNames
+			_numberOfFiles = _imageFiles.Count()
+			imageFilesSelectedLabel.Text = String.Format("{0} image files selected.", _numberOfFiles)
+			'For Each imageFile As String In _imageFiles
+			'	'MessageBox.Show(string.Format("You selected {0} image file!", imageFile.ToString()));
+			'	OutputThis2(String.Format(vbCr & " {0}", imageFile.ToString()))
+			'Next
+
+			'turn on transfer button now if the user selected some files:
+			If _numberOfFiles > 0 Then
+				btnTransfer.Enabled = True
+			End If
+		End If
+
+	End Sub
+
+	Private Sub btnTransfer_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnTransfer.Click
+		btnTransfer.Enabled = False
+		lblFilesInProgress.Visible = True
+		BackgroundWorker1.RunWorkerAsync(_sftpClient)
+	End Sub
+
+	Private Sub cbPictureTitle_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cbPictureTitle.SelectedIndexChanged
+		'set the folder name based on what picture type the user selected
+		'MessageBox.Show(cbPictureTitle.Items((cbPictureTitle.SelectedIndex)).ToString())
+		_pictureType = cbPictureTitle.Items((cbPictureTitle.SelectedIndex)).ToString()
+		'the folder name is something like this: 2013_Child_Photos_
+		_ftpPhotoFolderName = _pictureType.Replace(" ", "_") + "s_"
+		'MessageBox.Show(FormatFTPFolderName())
+	End Sub
+
+	Private Function FormatFTPFolderName() As String
+		Dim todayDate As String = DateTime.Now.ToShortDateString()
+		todayDate = todayDate.Replace("/"c, "_"c)
+		_ftpPhotoFolderName = String.Format(_ftpPhotoFolderName & "{0}", todayDate).Trim()
+		LogThis(String.Format(vbCr & "FTP folder for Photos will be: {0}", _ftpPhotoFolderName))
+		Return _ftpPhotoFolderName
+	End Function
+
+	Private Sub GetConfigEntries()
+		'sets the Photo FTP foldername using the App.config settings entries plus the type of picture title the user selected:
+		' the DEVworkingdirectory = /staging/21195D/ChildPhotos
+		' the PRODworkingdirectory = /Production/21195P/ChildPhotos
+		' the PRODPhotoImportFileLocation = "\\bbhcifs01\clientdata\Production\21195P\ChildPhotos"
+		' the DEVhotoImportFileLocation = "\\bbhcifs01\clientdata\staging\21195D\ChildPhotos"
+		'Const _host As String = "files.blackbaudhosting.com"
+		'Const _username As String = "MarkSe21195D@BBEC"
+		'Const _password As String = "P@ssword2"
+		'Const _workingdirectory As String = "/staging/21195D/ChildPhotos"
+		'Const _uploadfile As String = "C:\OCM\PhotoImports\2013 Photos_Processing_6_25_2013\_Import\C210608.jpg"
+		'Source Folder default location: C:\OCM\PhotoImports
+
+		' This will use an account specially purposed for these SFTP transfers:  OcMFTPUser21195D@BBEC and OcMFTPUser21195P@BBEC
+
+		'Dim ftpEnvironment As String = ConfigurationManager.AppSettings("FTPEnvironment")  'DEV, or PROD
+		Dim ftpEnvironment As String = My.Settings.FTPEnvironment	  'DEV, or PROD
+		If ftpEnvironment.ToLower().Equals("dev") Then
+			'_ftpCRMPhotoFileLocation = ConfigurationManager.AppSettings("DEV_BlackbaudPhotoFileLocation")
+			_ftpCRMPhotoFileLocation = My.Settings.DEV_BlackbaudPhotoFileLocation
+			_username = My.Settings.DEV_FTPUserName
+			_host = My.Settings.DEV_FTPHostName
+			_workingdirectory = My.Settings.DEV_FTPWorkingDirectory
+			_password = "P@ssword1"
+		Else
+			_ftpCRMPhotoFileLocation = My.Settings.PROD_BlackbaudPhotoFileLocation
+			_username = My.Settings.PROD_FTPUserName
+			_host = My.Settings.PROD_FTPHostName
+			_workingdirectory = My.Settings.PROD_FTPWorkingDirectory
+			_password = "P@ssword1"
+		End If
+		' this setting is same no matter what environment:
+		_ftpImportFileLocation = My.Settings.ImportFileLocation
+		_defaultSourceFolder = My.Settings.DefaultSourceFolder
+
+		'Dim output As String = String.Format("ftpEnvironment: {0} _ftpCRMPhotoFileLocation: {1} _username: {2} _workingdirectory: {3} _ftpImportFileLocation: {4}", ftpEnvironment, _ftpCRMPhotoFileLocation, _username, _workingdirectory, _ftpImportFileLocation)
+		'MessageBox.Show(output)
+
+	End Sub
+
 
 End Class
